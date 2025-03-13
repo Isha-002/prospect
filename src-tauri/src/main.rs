1bf7c86e434b8db8d97b02a5fcf86fba77b80f63
Isha-002 <narges1353zeinali@gmail.com>
2025-03-13 23:39:17 +0330

    implementing dialogs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,17 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
+use lessons::load::load_files;
+
+mod lessons;
+
+
+
+#[tokio::main]
+async fn main() {
+
+    let data = load_files().await.expect("Couldn't load lesson files");
+
+
     prospect_lib::run()
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,12 @@
 import { Outlet } from 'react-router';
 import Navbar from './Navbar';
+import { invoke } from '@tauri-apps/api/core';
 
 export default function Layout() {
+  const invoke = window.__TAURI__.core.invoke;
+
+  invoke('load_files')
+  
   return (
     <main className='h-screen'>
       <Navbar />
